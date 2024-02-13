829c9f771fb7368a840d8fd1847354a2b424c7e1
busra idil <diyetisyenbusraidil@gmail.com>
2024-02-13 21:16:02 +0300

    Class with attribute

---
+++ minivideos/classeswithattributes/Product.java
@@ -0,0 +1,11 @@
+package classeswithattributes;
+
+public class Product {
+
+    //attribute- field
+    int id;
+    String name;
+    String description;
+    double price;
+    int stockAmount;
+}
